bce4f69e7136374b645581357fd7ae3a8ca82d05
Rominavila47 <rominavila656@gmail.com>
2021-10-27 10:50:58 -0300

    correccion de calculadora

--- conferencia.js
+++ conferencia.js
@@ -1,7 +1,7 @@
 function calculo(){
-    var seleccion=parseFloat(document.getElementById ("categoria").value);
-    var valorSeleccionado=document.getElementById ("option").value;
-    var cantidad=document.getElementById ("cantidad").value;
+    var seleccion=document.getElementById("categoria");
+    var valorSeleccionado=  parseFloat(seleccion.options[seleccion.selectedIndex].value);
+    var cantidad=document.getElementById("cantidad").value;
     var total=0;
 
     if (seleccion=="" && valorSeleccionado=="" && cantidad=="" && total=="" )
@@ -11,19 +11,19 @@ function calculo(){
       }
     switch(valorSeleccionado){
         case 1:
-            total=(200*cantidad)+((200*cantidad)*80)/100
+            total=(200*cantidad)-((200*cantidad)*0.80)
             break;
         case 2:
-            total=(200*cantidad)+((200*cantidad)*50)/100
+            total=(200*cantidad)-((200*cantidad)*0.50)
             break;
         case 3:
-            total=(200*cantidad)+((200*cantidad)*15)/100
+            total=(200*cantidad)-((200*cantidad)*0.15)
             break;
         case 4:
             total=(200*cantidad)
             break;    
     }
-    document.getElementById("monto").value=total;
+    document.getElementById("monto").value= "Total a pagar: $" + total;
    // return document.getElementaryById("Total").innerHTML=resultado;
 
 
